a4d8da5af13857f4db9082bd43ee0da66b257004
Tony Batts <tonybatts@gmail.com>
2019-09-17 19:51:10 -0700

    Update simpsonsshuffler.py

--- simpsonsshuffler.py
+++ simpsonsshuffler.py
@@ -1,10 +1,24 @@
 !/Usr/Bin/Python
 
-import RPi.GPIO as GPIO import time import os import random
+import RPi.GPIO as GPIO 
+import time 
+import os 
+import random
+
 buttonPin = 17
-directory = "/home/stephen/simpsons/"
-GPIO.setmode(GPIO.BCM) GPIO.setup(buttonPin, GPIO.IN)
-def playEpisode(): episode = random.choice(os.listdir(directory)) cmd = "nohup omxplayer -b -o hdmi "+"'"+directory+episode+"' &" os.system('killall omxplayer.bin') os.system(cmd)
+
+directory = "/home/pi/simpsons/"
+
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(buttonPin, GPIO.IN)
+
+def playEpisode(): 
+	episode = random.choice(os.listdir(directory)) 
+	cmd = "nohup omxplayer -b -o hdmi "+"'"+directory+episode+"' &" 
+	os.system('killall omxplayer.bin') 
+	os.system(cmd)
+
+
 try:
 # measure the time the button is pressed as timeA
 GPIO.wait_for_edge(buttonPin, GPIO.RISING)
@@ -17,12 +31,13 @@ timeB = time.time()
 # take the first time away from the second time, to get the difference
 timeDifference = timeB - timeA
 
-# if the difference in times is more than 4 seconds, shutdown, or else play an episode and restart this script
-if timeDifference > 4:
+# if the difference in times is more than 3 seconds, shutdown, or else play an episode and restart this script
+if timeDifference > 3:
     os.system('sudo shutdown now')
 else:
     playEpisode()
     # point this to the location of this file
     os.system('sudo python /home/stephen/randomSimpsonsShutdown.py')
+
 except KeyboardInterrupt:
-GPIO.cleanup()
+	GPIO.cleanup()
